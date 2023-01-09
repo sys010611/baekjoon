a3c223ef78e3124be5bb54768b1713eb6a48f284
sys010611 <61095808+sys010611@users.noreply.github.com>
2023-01-09 18:28:36 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+long long dp[1000];
+
+long long Fibo(int n)
+{
+	for (int i = 3; i < n + 1; i++)
+	{
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+	}
+	return dp[n];
+}
+
+int main()
+{
+	dp[1] = 1; //base case
+	dp[2] = 2; //base case
+
+	int N;
+
+	cin >> N;
+
+	cout << Fibo(N);
+}
