4872a29a0106d5c43b6638b33435ef0cb830c19b
sys010611 <61095808+sys010611@users.noreply.github.com>
2023-02-01 19:46:03 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -3,47 +3,37 @@
 #include <algorithm>
 using namespace std;
 
-int N, S;
 int num[20];
-int currentSum = 0;
-int result=0;
+int currSum = 0;
+int result = 0;
+int n, s;
 
-void dfs(int currIndex);
+void backtrack(int currIndex);
 
 int main()
 {
-	cin >> N >> S;
-
-	for(int i=0;i<N;i++)
+	cin >> n >> s;
+	for (int i = 0; i < n; i++)
 	{
 		cin >> num[i];
 	}
 
-	dfs(0);
+	backtrack(0);
 	cout << result;
 }
 
-void dfs(int currIndex)
+void backtrack(int currIndex)
 {
-	if (currIndex == N) //주어진 숫자를 벗어났을 경우
-	{
+	if (currIndex == n)
 		return;
-		//cout << "인덱스 초과" << endl;
-	}
 
-	if (currentSum + num[currIndex] == S) //현재 총합 + 현재 숫자가 목표치일 경우 result +1
-	{
+	if (currSum + num[currIndex] == s)
 		result++;
-		//cout << "result++" << endl;
-	}
 
-	//현재 숫자를 더하지 않고 계속하기
-	dfs(currIndex+1);
+	backtrack(currIndex+1);
 
-	//현재 숫자를 더하고 계속하기
-	currentSum += num[currIndex];
-	dfs(currIndex+1);
-	
-	//끝까지 탐색했다면 이전 단계로 돌아가면서 직전에 더했던 값을 빼기
-	currentSum -= num[currIndex];
+	currSum += num[currIndex];
+	backtrack(currIndex+1);
+
+	currSum -= num[currIndex];
 }
