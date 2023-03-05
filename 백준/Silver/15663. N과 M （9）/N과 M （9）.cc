5a7c0808949ffa452511b2ab9a9cad011e2da6b7
sys010611 <61095808+sys010611@users.noreply.github.com>
2023-03-05 14:59:29 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 4072 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int n, m;
+vector<vector<int>> result;
+vector<int> currArr;
+int num[8];
+int visited[8];
+int lastNum;
+
+void backtrack()
+{
+	if (currArr.size() == m)
+	{
+		result.push_back(currArr); //현재 수열 저장
+		lastNum = currArr.back();
+		currArr.pop_back(); //이전 단계로
+		return;
+	}
+
+	lastNum = -1;
+	for (int i = 0; i < n; i++)
+	{
+		if (!visited[i] && lastNum != num[i])
+		{
+			visited[i] = true;
+			currArr.push_back(num[i]);
+			backtrack();
+			visited[i] = false;
+		}
+	}
+
+	if (currArr.size() > 0)
+	{
+		lastNum = currArr.back();
+		currArr.pop_back();
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) 
+		cin >> num[i];
+
+	sort(num, num + n);
+	fill(visited, visited + 8, false);
+
+	backtrack();
+
+	vector<vector<int>>::iterator it;
+	vector<int>::iterator it2;
+
+	for (it = result.begin(); it != result.end(); it++)
+	{
+		vector<int> currLine = *it;
+		for (it2 = currLine.begin(); it2 != currLine.end(); it2++)
+		{
+			cout << *it2 << " ";
+		}
+		cout << "\n";
+	}
+}
