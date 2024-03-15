68df5438a9f33fa76ca2e04379c187b185ebe4e3
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-03-15 09:25:30 +0900

    [Gold II] Title: 반도체 설계, Time: 12 ms, Memory: 10344 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <vector>
+using namespace std;
+
+int N;
+vector<int> A;
+
+int result = -1;
+
+int lastNumOnLength[1000001]; // 인덱스 i = 수열의 길이, 요소 : 수열의 마지막 수
+int arrToLis[1000001];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	fill(lastNumOnLength, lastNumOnLength + 1000001, 2000000000); //무한대로 초기화
+	fill(arrToLis, arrToLis + 1000001, -1); //
+	lastNumOnLength[0] = -2000000000; // 길이가 0인 수열의 마지막 수 : 무한소
+
+	cin >> N;
+
+	int input;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> input;
+		A.push_back(input);
+	}
+
+	for (int i = 0; i < A.size(); i++)
+	{
+		int currNum = A[i];
+		//currNum이 붙어서 될 수 있는 최장길이 탐색
+		int possibleLength = lower_bound(lastNumOnLength, lastNumOnLength + 1000001, currNum) - lastNumOnLength;
+
+		/*cout << "CurrNum: " << currNum << endl;
+		cout << "possibleLength : " << possibleLength << endl;*/
+
+		// 길이가 k인 수열의 마지막 숫자를 최대한 작게 유지.
+		if (lastNumOnLength[possibleLength] > A[i])
+			lastNumOnLength[possibleLength] = A[i];
+
+		if (possibleLength > result)
+			result = possibleLength;
+
+		arrToLis[i] = possibleLength; // A에서 i번째인 수는 LIS에서는 possibleLength번째에 있음.
+	}
+
+	cout << result;
+}
