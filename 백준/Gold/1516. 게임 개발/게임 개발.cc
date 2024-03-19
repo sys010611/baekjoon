6f4c77fcf8baa50d0dbcddc6bab900450ea3e77f
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-03-19 17:27:36 +0900

    [Gold III] Title: 게임 개발, Time: 24 ms, Memory: 2420 KB -BaekjoonHub

---
+++ 백준/Gold/1516. 게임 개발/게임 개발.cc
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+int N;
+vector<int> indegree;
+vector<vector<int>> adjList;
+vector<int> cost;
+
+void PrintGraph()
+{
+	cout << "adjList: " << endl;
+	for (int i = 1; i <= N; i++)
+	{
+		cout << i << "->";
+		for (auto it = adjList[i].begin(); it != adjList[i].end(); it++)
+		{
+			cout << *it << ", ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	cout << "indegree : " << endl;
+	for (int i = 1; i <= N; i++)
+	{
+		cout << i << ": " << indegree[i] << endl;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N;
+
+	adjList.resize(N + 1);
+	indegree.resize(N + 1);
+	fill(indegree.begin(), indegree.end(), 0);
+	cost.resize(N + 1);
+
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> cost[i];
+
+		int input = 0;		
+		while (true)
+		{
+			cin >> input;
+
+			if (input == -1)
+				break;
+			else
+			{
+				adjList[input].push_back(i);
+				indegree[i]++;
+				continue;
+			}
+		}
+	}
+
+	//PrintGraph();
+
+	vector<int> result;
+	result.resize(N+1);
+
+	//위상 정렬
+	queue<int> Q;
+	for (int i = 1; i <= N; i++)
+	{
+		if (indegree[i] == 0)
+		{
+			Q.push(i);
+			result[i] = cost[i];
+		}
+	}
+
+	for(int i=0;i<N;i++)
+	{
+		if (Q.empty()) // 위상정렬 불가
+		{
+			cout << 0;
+			return 0;
+		}
+
+		int curr = Q.front(); Q.pop();
+
+		for (int adjNode : adjList[curr])
+		{
+			indegree[adjNode] -= 1;
+
+			if (indegree[adjNode] == 0)
+			{
+				Q.push(adjNode);
+			}
+
+			result[adjNode] = max(result[adjNode], result[curr] + cost[adjNode]);
+		}
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		cout << result[i] << endl;
+	}
+}
