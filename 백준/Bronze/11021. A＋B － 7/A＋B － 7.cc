70502deddc08ce1ca4f70352dac856073817138b
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-04-07 22:27:41 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t;
+	cin >> t;
+	for (int i = 1; i <= t; i++)
+	{
+		int a,b;
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a + b << endl;
+	}
+}
+
+
