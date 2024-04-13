c12acfb298b31dbec1ca4088552f23544af086cc
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-04-14 08:22:35 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+typedef pair<int,int> P;
+
+int N, K, L;
+deque<pair<int, char>> controls;
+int board[100][100];
+deque<pair<int,int>> snake;
+
+int dRow[] = {0,1,0,-1};
+int dCol[] = {1,0,-1,0};
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << "강한친구 대한육군" << endl << "강한친구 대한육군";
+}
+
+
