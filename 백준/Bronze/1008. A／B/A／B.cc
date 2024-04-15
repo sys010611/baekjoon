a3f5e0b8742069942596f1678d1cb881e18b2252
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-04-16 08:31:04 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	double a,b;
+	cin >> a >> b;
+
+
+	cout << fixed;
+	cout.precision(10);
+
+	cout << a/b;
+}
+
+
