0b7cd1935b0f90df7f33c2b902e8dda199860dca
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-05-07 22:50:21 +0900

    [Gold V] Title: 강의실 배정, Time: 64 ms, Memory: 5224 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+int N;
+vector<pair<int,int>> classes;
+
+bool compare(pair<int, int> a, pair<int, int> b)
+{
+	return a.first < b.first;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int s,t;
+		cin >> s >> t;
+		classes.push_back({s,t});
+	}
+
+	sort(classes.begin(), classes.end(), compare);
+
+	priority_queue<int, vector<int>, greater<int>> finishingTimes;
+	finishingTimes.push(0);
+
+	int result = 0;
+	for (auto item : classes)
+	{
+		int minFinishingTime = finishingTimes.top(); 
+		if (minFinishingTime <= item.first) // 강의실을 이어서 쓸 수 있을 경우
+		{
+			finishingTimes.pop();
+			finishingTimes.push(item.second);
+		}
+		else
+		{
+			result++;
+			finishingTimes.push(item.second);
+		}
+	}
+
+	cout << finishingTimes.size();
+}
