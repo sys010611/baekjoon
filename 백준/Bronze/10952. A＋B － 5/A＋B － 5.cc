84bf20c0a8f3d60afe9a6d19226dc92b64000cc2
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-05-21 18:16:31 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,16 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    while(true){
+        int a,b;cin>>a>>b;
+    
+        if(a==0&&b==0) break;
+        
+        cout<<a+b<<endl;
+    }
+    
+
+    return 0;
+}
