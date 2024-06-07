8bc40fd29209289765097c0ebd19aff6341b5357
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-06-07 10:54:01 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cstring>
+using namespace std;
+int main()
+{
+	int t; cin >> t;
+	while (t--)
+	{
+		int a, b;
+		cin >> a >> b;
+		cout << a + b << endl;
+	}
+}
