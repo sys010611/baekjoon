35375c3e7b3558c352b00a55a563ae4ae4f2a356
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-06-13 11:36:31 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 224 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t;cin>>t;
+	while (t--)
+	{
+		int a,b; cin >> a >> b; cout << a+b<<'\n';
+	}
+}
