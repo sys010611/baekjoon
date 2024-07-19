2a81492c13fa785152b3f8c00e2b92eec79c0517
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-07-20 01:57:54 +0900

    [Bronze III] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+	int n;cin>>n;
+	int result=1;
+	for(int i=1;i<=n;i++)	 result *= i;
+	cout << result;
+}
