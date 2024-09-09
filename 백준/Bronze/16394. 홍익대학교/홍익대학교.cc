699f0989623452feb6c0a6b25ee63257fc1fbef9
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-09-09 16:09:57 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >>n;
+
+	cout << n-1946;
+}
