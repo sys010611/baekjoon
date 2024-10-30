95286c9dd48689d4f58e682e816e15ba72f7c8e8
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-10-30 13:33:47 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 48 ms, Memory: 6816 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,143 +1,70 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <cstring>
+#include <queue>
+
 using namespace std;
 
-class Edge
-{
-public:
-	int cost;
-	int v1;
-	int v2;
-	bool included;
+typedef pair<int,int> P;
 
-	Edge(int v1, int v2, int cost): cost(cost), v1(v1), v2(v2), included(false) {};
-};
+vector<vector<P>> adjList; // cost, nodeNum
+bool visited[10001];
 
-bool CompareEdge(Edge& e1, Edge& e2)
+int V, E;
+
+int Prim()
 {
-	return e1.cost < e2.cost;
+	int result = 0;
+
+	priority_queue<P, vector<P>, greater<P>> PQ;
+
+	PQ.push(P(0, 1));
+
+	while (!PQ.empty())
+	{
+		P curr = PQ.top(); PQ.pop();
+
+		int currCost = curr.first;
+		int currNode = curr.second;
+
+		if (visited[currNode])
+		{
+			continue;
+		}
+		visited[currNode] = true;
+
+		result += currCost;
+
+		for (auto& next : adjList[currNode])
+		{
+			if (!visited[next.second])
+			{
+				PQ.push(next);
+			}
+		}
+	}
+
+	return result;
 }
 
-class Graph
+signed main()
 {
-public:
-	int N; // 노드의 개수
-	vector<Edge> edgeList;
-
-	vector<int> root;
-	
-	Graph(int n) : N(n)
-	{
-		root.resize(N+1);
-		fill(root.begin(), root.end(), -1);
-	}
-
-	void AddEdge(int u, int v, int cost)
-	{
-		edgeList.push_back(Edge(u, v, cost));
-	}
-
-	int getRoot(int node)
-	{
-		if (root[node] == -1)
-		{
-			return node;
-		}
-		else
-		{
-			return getRoot(root[node]);
-		}
-	}
-
-	/// <summary>
-	/// 두 정점이 같은 집합에 속해있는지
-	/// </summary>
-	bool find(int v1, int v2)
-	{
-		int root1 = getRoot(v1);
-		int root2 = getRoot(v2);
-
-		if (root1 == root2)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	void merge(int v1, int v2)
-	{
-		int root1 = getRoot(v1);
-		int root2 = getRoot(v2);
-
-		root[root2] = root1;
-	}
-
-	int Kruskal()
-	{
-		sort(edgeList.begin(), edgeList.end(), CompareEdge);
-
-		//for (auto it = edgeList.begin(); it != edgeList.end(); it++)
-		//{
-		//	cout << it->v1 << " " << it->v2 << " " << it->cost << endl;
-		//}
-
-		int edgeCount = 0;
-		long long treeCost = 0;
-		int idx = 0;
-
-		for (auto it = edgeList.begin(); it != edgeList.end(); it++)
-		{
-			//cout << "현재 간선:  " << it->v1 << " " << it->v2 << endl;
-
-			//cout << "루트 현황: " << endl;
-			//for (auto it = root.begin(); it != root.end(); it++)
-			//{
-			//	cout << *it << " ";
-			//}
-			//cout << endl;
-
-			if (find(it->v1, it->v2))
-			{
-				//cout << "루트 같음, 유니온 X" << endl;
-				continue;
-			}
-			else
-			{
-				//cout << "유니온 수행  " << it->v1 << " " << it->v2 << endl;
-				merge(it->v1, it->v2);
-				edgeCount++;
-				treeCost += it->cost;
-				it->included = true;
-			}
-
-			if(edgeCount == N-1)
-				break;
-		}
-
-		return treeCost;
-	}
-};
-
-int main()
-{
-	int V, E;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
 	cin >> V >> E;
 
-	Graph G(V);
+	adjList.resize(V+1);
 
-	int A,B,C;
 	for (int i = 0; i < E; i++)
 	{
-		cin >> A >> B >> C;
+		int a,b,c;
+		cin>>a>>b>>c;
 
-		G.AddEdge(A,B,C);
+		adjList[a].push_back(P(c, b));
+		adjList[b].push_back(P(c, a));
 	}
 
-	//Edge들을 줄세운 다음 비용이 작은 것 부터 하나씩 포함시키기
-	cout << G.Kruskal();
+	cout << Prim();
 }
