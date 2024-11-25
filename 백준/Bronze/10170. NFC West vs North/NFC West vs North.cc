ed226190ff337c575e99afabf0271fe6acc121e4
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-11-25 16:18:17 +0900

    [Bronze V] Title: NFC West vs North, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <set>
+#include <queue>
+using namespace std;
+
+string str = "NFC West       W   L  T\n\
+-----------------------\n\
+Seattle        13  3  0\n\
+San Francisco  12  4  0\n\
+Arizona        10  6  0\n\
+St. Louis      7   9  0\n\
+\n\
+NFC North      W   L  T\n\
+-----------------------\n\
+Green Bay      8   7  1\n\
+Chicago        8   8  0\n\
+Detroit        7   9  0\n\
+Minnesota      5  10  1";
+
+signed main()
+{
+	cout << str;
+}
