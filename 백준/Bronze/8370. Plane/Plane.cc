e34d891169f8431bd44027ab13ab5fc5bd47537c
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-12-08 12:28:02 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <stack>
+#include <queue>
+using namespace std;
+
+
+signed main()
+{
+	int n1, k1, n2, k2;
+	cin >> n1 >> k1 >> n2 >> k2;
+	cout << n1 * k1 + n2 * k2;
+}
