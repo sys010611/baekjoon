1c3a80b46ea042179f84d09e2aeaaa7fc821590b
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-12-13 20:08:37 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N;
+	cin >> N;
+	while (N--)
+	{
+		cout << "LoveisKoreaUniversity" << " ";
+	}
+}
