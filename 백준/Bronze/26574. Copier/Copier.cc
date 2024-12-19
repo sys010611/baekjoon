a90e309aedd18c8ed0a7e9fab6f7a0fe4c6c6969
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-12-19 18:53:20 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <climits>
+#include <numeric>
+using namespace std;
+
+
+signed main()
+{
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		int input; cin >> input;
+		cout << input << " " << input << endl;
+	}
+}
