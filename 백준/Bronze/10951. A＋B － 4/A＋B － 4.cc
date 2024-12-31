5142005740fc1d56df643d6dedebb003c221b322
sys010611 <61095808+sys010611@users.noreply.github.com>
2024-12-31 23:29:37 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <climits>
+#include <cstring>
+#include <set>
+using namespace std;
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int a, b;
+	while (cin >> a >> b)
+	{
+		cout << a + b << endl;
+	}
+}
+
