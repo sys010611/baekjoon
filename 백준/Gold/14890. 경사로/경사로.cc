91fb1dd5337b22fc2c9ec8e0b5eabd98099804a8
sys010611 <61095808+sys010611@users.noreply.github.com>
2025-01-21 14:08:56 +0900

    [Gold III] Title: 경사로, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/14890. 경사로/경사로.cc
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int N, L;
+int grid[100][100];
+
+bool CheckLine(vector<int>& line)
+{
+	vector<bool> placed(N);
+	fill(placed.begin(), placed.end(), false);
+
+	for (int i = 0; i < N - 1; i++)
+	{
+		int curr = line[i];
+		int next = line[i + 1];
+
+		if (curr != next)
+		{
+			if (curr + 1 == next)
+			{
+				int startIdx = i - L + 1;
+
+				if (startIdx < 0)
+					return false;
+
+				int start = line[startIdx];
+				if (start == curr)
+				{
+					// 배치해보기
+					for (int j = startIdx; j <= i; j++)
+					{
+						if (line[j] != curr)
+							return false;
+
+						if (placed[j])
+							return false;
+
+						placed[j] = true;
+					}
+				}
+				else
+					return false;
+			}
+			else if (curr - 1 == next)
+			{
+				int endIdx = i + L;
+				if (endIdx >= N)
+					return false;
+
+				int end = line[endIdx];
+
+				// 배치해보기
+				for (int j = i + 1; j <= endIdx; j++)
+				{
+					if (line[j] != next)
+						return false;
+
+					if (placed[j])
+						return false;
+
+					placed[j] = true;
+				}
+
+			}
+			else
+				return false;
+		}
+	}
+	return true;
+}
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N >> L;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> grid[i][j];
+		}
+	}
+
+	int ret = 0;
+	for (int i = 0; i < N; i++)
+	{
+		vector<int> rowLine, colLine;
+		for (int j = 0; j < N; j++)
+		{
+			rowLine.push_back(grid[i][j]);
+			colLine.push_back(grid[j][i]);
+		}
+		if (CheckLine(rowLine))
+			ret++;
+		if (CheckLine(colLine))
+			ret++;
+	}
+	cout << ret;
+}
