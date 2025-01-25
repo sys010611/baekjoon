8d0affcda68f782cfd5397353b152c932c64ebb8
sys010611 <61095808+sys010611@users.noreply.github.com>
2025-01-25 10:50:41 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n; cin >> n;
+	cout << n * (n - 1);
+}
