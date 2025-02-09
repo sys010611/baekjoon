9989b30e482797dfd1b090572370523d5185dafd
sys010611 <61095808+sys010611@users.noreply.github.com>
2025-02-09 16:28:15 +0900

    [Bronze V] Title: Just Round Down, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	float n;
+	cin >> n;
+	n = floor(n);
+	cout << n;
+}
