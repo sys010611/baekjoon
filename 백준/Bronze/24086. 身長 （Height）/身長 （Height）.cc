61eadbeb32b365c5a937e61d1233cd9ae0f9fcfc
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-02-21 13:56:39 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <cassert>
+#include <queue>
+using namespace std;
+
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int a, b; cin >> a >> b; cout << b - a;
+}
