0462cbd3b3f49bf018dfc5fca652992596814f2a
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-03-03 14:01:10 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <cassert>
+#include <queue>
+#include <climits>
+using namespace std;
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+
+	cout << "파이팅!!";
+}
