7f89539e9ff8aa394b730e66fa5886269d7ceac2
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-03-07 22:01:51 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <cassert>
+#include <queue>
+#include <climits>
+#include <cmath>
+using namespace std;
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		cout << "SciComLove" << endl;
+	}
+}
