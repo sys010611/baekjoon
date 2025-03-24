1f890f2659b9c908a1cf1b9f9762828ef5ff5cfd
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-03-24 16:33:29 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <bitset>
+#include <queue>
+#include <climits>
+#include <cmath>
+#include <stack>
+using namespace std;
+
+int dp[5002]; // n번째 자리까지 고려했을 때 가능한 경우의 수
+#define DIV 1000000
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+    string s = R"(.  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/.)";
+    cout << s;
+}
