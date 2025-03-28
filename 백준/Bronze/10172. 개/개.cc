7dd856b66ae108adf22f60704cf533ff1066beb8
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-03-28 15:04:27 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <climits>
+using namespace std;
+
+signed main()
+{
+	string s;
+	s = R"###(|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|)###";
+	cout << s;
+}
