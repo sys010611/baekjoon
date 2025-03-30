6d4406fe6ec79f0e019db8c498c5b1c631104c01
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-03-30 17:00:45 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <bitset>
+#include <queue>
+#include <climits>
+#include <cmath>
+#include <stack>
+using namespace std;
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string s;
+    s = R"(     /~\
+    ( oo|
+    _\=/_
+   /  _  \
+  //|/.\|\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |)";
+    cout << s;
+}
