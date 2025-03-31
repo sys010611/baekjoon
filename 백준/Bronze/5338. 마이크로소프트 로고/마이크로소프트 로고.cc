e15c91db790dbecee11cb0f115e98466a693e345
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-03-31 10:33:34 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <climits>
+using namespace std;
+
+signed main()
+{
+	string s;
+	s = R"###(       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|)###";
+	cout << s;
+}
