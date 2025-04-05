b41d983501d474df7073e1898d8fc2a45b39105b
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-04-05 15:12:52 +0900

    [Bronze V] Title: Serca, Time: 2548 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <bitset>
+#include <queue>
+#include <climits>
+#include <cmath>
+#include <stack>
+using namespace std;
+
+#define int long long
+
+int N, M;
+int A[100001];
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n; cin >> n;
+	while (n--)
+	{
+		string s = R"( @@@   @@@ 
+@   @ @   @
+@    @    @
+@         @
+ @       @ 
+  @     @  
+   @   @   
+    @ @    
+     @    )";
+			cout << s << endl;
+	}
+}
