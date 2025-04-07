d0b9de28a417979ad79368bc146bf34263502b86
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-04-07 12:17:08 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <bitset>
+#include <queue>
+#include <climits>
+#include <cmath>
+#include <stack>
+using namespace std;
+
+#define int long long
+
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int a, b; cin >> a >> b; cout << a * b;
+}
