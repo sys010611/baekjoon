27739fdc8940835211718094ec8d5486ba47e9da
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-04-08 11:57:54 +0900

    [Bronze V] Title: 余り (Remainder), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <bitset>
+#include <queue>
+#include <climits>
+#include <cmath>
+#include <stack>
+using namespace std;
+
+#define int long long
+
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n; cin >> n;
+	cout << n % 21;
+}
