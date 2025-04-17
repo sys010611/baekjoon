06e643a4bb44c6d5a36583edc416c1a29bb57f2a
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-04-17 12:00:16 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 16 ms, Memory: 2948 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
@@ -1,74 +1,57 @@
 #include <iostream>
-#include <queue>
-#include <cstring>
-#include <algorithm>
+#include <vector>
+#include <string>
+#include <climits>
 using namespace std;
 
-string S;
-int dp[2501][2501];
-int F[2501];
+int main() {
+    string s;
+    cin >> s;
+    int n = s.size();
 
-#define INF 1000000
+    // 팰린드롬 여부를 저장하는 2차원 배열
+    vector<vector<bool>> isPalindrome(n, vector<bool>(n, false));
 
-int DP(int s, int e)
-{
-	if (s > e)
-		return 0;
+    // 모든 길이가 1인 문자열은 팰린드롬
+    for (int i = 0; i < n; ++i) {
+        isPalindrome[i][i] = true;
+    }
 
-	if (dp[s][e] != -1)
-		return dp[s][e];
+    // 길이가 2인 문자열도 팰린드롬 체크
+    for (int i = 0; i < n - 1; ++i) {
+        if (s[i] == s[i + 1]) {
+            isPalindrome[i][i + 1] = true;
+        }
+    }
 
-	// base case
-	if (s == e)
-	{
-		return dp[s][e] = 1;
-	}
-	else if (s + 1 == e)
-	{
-		if (S[s] == S[e])
-			return dp[s][e] = 1;
-		else
-			return dp[s][e] = 0;
-	}
+    // 길이가 3 이상인 문자열에 대해서 팰린드롬 여부 계산
+    for (int len = 3; len <= n; ++len) {
+        for (int i = 0; i <= n - len; ++i) {
+            int j = i + len - 1;
+            if (s[i] == s[j] && isPalindrome[i + 1][j - 1]) {
+                isPalindrome[i][j] = true;
+            }
+        }
+    }
 
-	if (S[s] == S[e])
-	{
-		return dp[s][e] = DP(s+1, e-1);
-	}
-	else
-	{
-		return dp[s][e] = 0;
-	}
+    // DP 배열
+    vector<int> dp(n, INT_MAX);
+
+    // 처음부터 i까지 팰린드롬으로 분할한 최소 횟수 계산
+    for (int i = 0; i < n; ++i) {
+        if (isPalindrome[0][i]) {
+            dp[i] = 0;  // 첫 번째부터 i까지가 팰린드롬이라면 분할 횟수는 0
+        } else {
+            for (int j = 0; j < i; ++j) {
+                if (isPalindrome[j + 1][i]) {
+                    dp[i] = min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+    }
+
+    // 출력: 최솟값 + 1 (dp[i]는 부분 문자열의 개수를 세므로)
+    cout << dp[n - 1] + 1 << endl;
+
+    return 0;
 }
-
-int Check(int e)
-{
-	if (e < 0)
-		return 0;
-
-	if (F[e] != INF)
-		return F[e];
-
-	for (int i = 0; i <= e; i++)
-	{
-		if (DP(i, e) == 1)
-		{
-			F[e] = min(F[e], 1 + Check(i - 1));
-		}
-	}
-	return F[e];
-}
-
-signed main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	cin >> S;
-
-	memset(dp, -1, sizeof(dp));
-	fill(F, F + 2501, INF);
-
-	cout << Check(S.length() - 1);
-}
-
