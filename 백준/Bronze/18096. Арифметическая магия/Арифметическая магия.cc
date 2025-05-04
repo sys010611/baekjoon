3d6b338341724cebfe591cff092cce6003d52ec5
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-05-04 10:42:21 +0900

    [Bronze V] Title: Арифметическая магия, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <climits>
+using namespace std;
+
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cout << 1;
+}
