a595cb4d482dbbf2b4025b44340918f278ebfed2
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-05-06 13:00:41 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <climits>
+using namespace std;
+
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n; cin >> n; 
+    n++;
+    cout << n * 2 << " " << n * 3;
+}
