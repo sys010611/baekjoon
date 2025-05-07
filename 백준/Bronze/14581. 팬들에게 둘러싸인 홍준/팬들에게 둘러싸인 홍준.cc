453ae5952a9aee6cd1704e2df1f4c312ecf3a505
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-05-07 16:27:03 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <climits>
+using namespace std;
+
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string s; cin >> s;
+
+    cout << ":fan::fan::fan:" << endl;
+    cout << ":fan::"<< s << "::fan:" << endl;
+    cout << ":fan::fan::fan:";
+}
