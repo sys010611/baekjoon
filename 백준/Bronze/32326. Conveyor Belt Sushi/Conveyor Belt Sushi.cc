5ff228f3c255e28d261977d9bd194e78568f2af4
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-05-08 19:00:58 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <climits>
+using namespace std;
+
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int a, b, c; cin >> a >> b >> c;
+    cout << a * 3 + b * 4 + c * 5;
+}
