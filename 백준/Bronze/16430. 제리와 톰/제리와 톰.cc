4ac0a01cfe0520766454c8b12a6acf097eea46d6
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-05-09 15:01:56 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <climits>
+using namespace std;
+
+int price[11];
+
+signed main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << b-a << " " << b;
+}
