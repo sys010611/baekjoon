d9c9339870a5b579faf2faa96e45e81ceeaf91a5
Yunseop Shin <61095808+sys010611@users.noreply.github.com>
2025-05-15 22:06:12 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <climits>
+#include <map>
+using namespace std;
+
+map<int, int> m;
+
+int solution()
+{
+
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int a, b; cin >> a >> b; cout << min(a, b);
+}
